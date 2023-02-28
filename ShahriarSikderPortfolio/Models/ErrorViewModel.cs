62e3265d81d7210ca0928810fdf07d4e0dc774a7
SHAHRIAR <SHAHRIAR@DESKTOP-UHR3647>
2023-03-01 01:13:04 +0600

    Add project files.

---
+++ ShahriarSikderPortfolio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShahriarSikderPortfolio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
